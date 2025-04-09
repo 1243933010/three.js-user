a2e03191b5fff28105b4bc785c80d0b7b014e309
ft221496 <ft221496@sf.com>
2025-04-09 19:50:17 +0800

    feat(): 修改阴影逻辑

--- html/src/shadows/js/index.js
+++ html/src/shadows/js/index.js
@@ -1,8 +1,25 @@
 
 import * as THREE from '../../../../three.module.js';   //module文件内部引入的有core.js,需要一起拿出来
 // import { OrbitControls } from '../../../../OrbitControls.js';
-import { resizeRendererToDisplaySize } from '../../../utils/utils.js'
+import { resizeRendererToDisplaySize, MinMaxGUIHelper, ColorGUIHelper } from '../../../utils/utils.js';
+import { GUI } from '../../../../lil-gui.module.min.js';
 
+
+
+export const initCamera = ({ camera }) => {
+    console.log('initCamera', camera)
+    camera.position.set(20, 15, -15);
+    camera.lookAt(0, 0, 0);
+
+    // const gui = new GUI();  // 摄像机控制器
+    // // gui.add(camera, 'fov', 1, 180).onChange(updateCamera);
+    // gui.add(camera, 'fov', 1, 180);
+    // const minMaxGUIHelper = new MinMaxGUIHelper(camera, 'near', 'far', 0.1);
+    // // gui.add(minMaxGUIHelper, 'min', 0.1, 50, 0.1).name('near').onChange(updateCamera);
+    // // gui.add(minMaxGUIHelper, 'max', 0.1, 50, 0.1).name('far').onChange(updateCamera);
+    // gui.add(minMaxGUIHelper, 'min', 0.1, 50, 0.1).name('near');
+    // gui.add(minMaxGUIHelper, 'max', 0.1, 50, 0.1).name('far');
+}
 export const plainGenerate = ({ scene, loader, renderer, camera }) => {
     // 地板
     const planeSize = 40;
@@ -13,7 +30,7 @@ export const plainGenerate = ({ scene, loader, renderer, camera }) => {
     const repeats = planeSize / 2;
     texture.repeat.set(repeats, repeats);
     const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
-    const planeMat = new THREE.MeshBasicMaterial({
+    const planeMat = new THREE.MeshPhongMaterial({
         map: texture,
         side: THREE.DoubleSide,
     });
@@ -26,13 +43,32 @@ export const plainGenerate = ({ scene, loader, renderer, camera }) => {
 
 export const lightGenerate = ({ scene, loader, renderer, camera }) => {
     // 灯光
-    const skyColor = 0xb1e1ff; // light blue
-    const groundColor = 0xb97a20; // brownish orange
-    const intensity = 5;
-    const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
-    // const light = new THREE.DirectionalLight(0xb1e1ff, 1);
+    // const skyColor = 0xb1e1ff; // light blue
+    // const groundColor = 0xb97a20; // brownish orange
+    // const intensity = 5;
+    // const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);  //环境光，没有特定方向不显示阴影
+    const light = new THREE.DirectionalLight(0xb3adad, 5);  //平行光，有特定方向显示阴影
     light.castShadow = true;
+    light.position.set(5, 10, 2);
+
+    // light.shadow.mapSize.width = 1024;
+    // light.shadow.mapSize.height = 1024;
+    // light.shadow.camera.near = 0.5;
+    // light.shadow.camera.far = 50;
     scene.add(light);
+
+    const helper = new THREE.DirectionalLightHelper(light);  // 点光源辅助,显示光源位置
+    scene.add(helper);
+    
+
+    const gui = new GUI();  //快捷操作控制台设置
+    gui.addColor(new ColorGUIHelper(light, 'color'), 'value').name('color');
+    gui.add(light, 'intensity', 0, 5, 0.01);
+    gui.add(light.target.position, 'x', -10, 10);
+    gui.add(light.target.position, 'z', -10, 10);
+    gui.add(light.target.position, 'y', 0, 10);
+    makeXYZGUI(gui, light.position, 'position', updateLight(helper));
+    makeXYZGUI(gui, light.target.position, 'target', updateLight(helper));
 
 }
 
@@ -47,7 +83,9 @@ export const sphereGenerate = ({ scene, loader, renderer, camera }) => {
     );
     const sphereMat = new THREE.MeshPhongMaterial({
         color: 0xffffff,
-        wireframe: true,
+        // wireframe: true,
+        flatShading: true,
+        side: THREE.DoubleSide,
     });
     const mesh = new THREE.Mesh(sphereGeo, sphereMat);
     mesh.castShadow = true;
@@ -64,6 +102,7 @@ export const boxGenerate = ({ scene, loader, renderer, camera }) => {
     const boxMat = new THREE.MeshPhongMaterial({
         color: 0xffffff,
         flatShading: true,
+        side: THREE.DoubleSide,
     });
     const mesh = new THREE.Mesh(boxGeo, boxMat);
     mesh.castShadow = true;
@@ -82,4 +121,17 @@ export const renderFnc = ({ scene, loader, renderer, camera }) => {
     }
     renderer.render(scene, camera);
 
+}
+
+
+function makeXYZGUI(gui, vector3, name, onChangeFn) {
+    const folder = gui.addFolder(name);
+    folder.add(vector3, 'x', -10, 10).onChange(onChangeFn);
+    folder.add(vector3, 'y', 0, 10).onChange(onChangeFn);
+    folder.add(vector3, 'z', -10, 10).onChange(onChangeFn);
+    folder.open();
+}
+
+function updateLight(helper) {
+    helper.update();
 }
--- html/utils/utils.js
+++ html/utils/utils.js
@@ -16,14 +16,52 @@ export const resizeRendererToDisplaySize = (renderer) => {  // resize the render
 }
 
 
-export const main = (callback, id = 'c') => {
+export const main = (callback, id = 'app') => {
     const canvas = document.querySelector(`#${id}`);
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(45, 2, 0.1, 100)
+    const camera = null;
     const loader = new THREE.TextureLoader();
     const renderer = new THREE.WebGLRenderer({   //渲染器
         canvas, antialias: true
     });
- 
-    callback({ canvas, scene, renderer,loader, camera })
+
+    callback({ canvas, scene, renderer, loader, camera })
+}
+
+
+export class MinMaxGUIHelper {  // 最小最大辅助类
+    constructor(obj, minProp, maxProp, minDif) {
+        this.obj = obj;
+        this.minProp = minProp;
+        this.maxProp = maxProp;
+        this.minDif = minDif;
+    }
+    get min() {
+        return this.obj[this.minProp];
+    }
+    set min(v) {
+        this.obj[this.minProp] = v;
+        this.obj[this.maxProp] = Math.max(this.obj[this.maxProp], v + this.minDif);
+    }
+    get max() {
+        return this.obj[this.maxProp];
+    }
+    set max(v) {
+        this.obj[this.maxProp] = v;
+        this.min = this.min;  // 这将调用min的setter
+    }
+}
+
+
+export class ColorGUIHelper {  // 颜色辅助类
+    constructor(object, prop) {
+        this.object = object;
+        this.prop = prop;
+    }
+    get value() {
+        return `#${this.object[this.prop].getHexString()}`;
+    }
+    set value(hexString) {
+        this.object[this.prop].set(hexString);
+    }
 }
